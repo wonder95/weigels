b27d4b912996491a0f06d80b1a2a76f3ffdc09df
wonder95 <steve@smgaweb.com>
2022-08-13 15:03:38 -0700

    Also remove Netlify code from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 import image from "@astrojs/image";
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind(), image()],
-  output: 'server',
-  adapter: netlify()
 });
