911308808dd4dbd2d48052c15096009dc7440182
wonder95 <steve@smgaweb.com>
2022-08-12 22:56:54 -0700

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 import image from "@astrojs/image";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), image()]
+  integrations: [vue(), tailwind(), image()],
+  output: 'server',
+  adapter: netlify()
 });
