5ad6a3469263662d2f98da850d0a68b39b854cae
wonder95 <steve@smgaweb.com>
2022-08-13 22:49:03 -0700

    Re-install @astrojs/netlify
    since build is filing without it for some reason

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
+import image from "@astrojs/image";\
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
