eedb324c4122c428617714263858a5a979147a04
wonder95 <steve@smgaweb.com>
2022-12-14 09:00:40 -0800

    Add env var for Prismic key

--- src/prismic.js
+++ src/prismic.js
@@ -2,7 +2,8 @@ import * as prismic from '@prismicio/client';
 
 const apiEndpoint = 'https://weigels-custom-laser-engravng.prismic.io/api/v2';
 
-const Client = new prismic.Client(apiEndpoint);
+const Client = new prismic.Client(apiEndpoint, { accessToken: import.meta.env.PRISMIC_ACCESS_TOKEN });
+// const Client = new prismic.Client(apiEndpoint);
 
 export function getService(slug) {
     return Client.getByUID('service', slug);
@@ -10,8 +11,7 @@ export function getService(slug) {
 
 export async function getFrontPage() {
     const client = await Client.getByType('front_page');
-    return client;
-    
+    return client;    
 };
 
 export async function getAllServices() {
